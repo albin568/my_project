5e5fd21eeb381a43c5f44a4a994ce371df2bd6e6
Vijayleo46 <vijaymartin72@gmail.com>
2023-08-03 15:38:00 +0530

    03082023

--- lib/Felis_Catus/screens/profile/profile.dart
+++ lib/Felis_Catus/screens/profile/profile.dart
@@ -13,11 +13,54 @@ class _ProfilePageState extends State<ProfilePage> {
     return Scaffold(
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextButton(onPressed: () {},
-                child: const Text('Name'))
-          ],
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 50,
+                width: 350,
+                child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: OutlinedButton(onPressed: () {},
+                      child: const Text("Owner's Name")),
+                ),
+              ),
+              SizedBox(
+                height: 50,
+                width: 350,
+                child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: OutlinedButton(onPressed: () {},
+                      child: const Text("Pet's Name")),
+                ),
+              ),
+              SizedBox(
+                height: 50,
+                width: 350,
+                child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: OutlinedButton(onPressed: () {},
+                      child: const Text("Pet's Age")),
+                ),
+              ),
+              SizedBox(
+                height: 50,
+                width: 350,
+                child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: OutlinedButton(onPressed: () {},
+                      child: const Text("Address")),
+                ),
+              ),
+              SizedBox(
+                height: 50,
+                width: 350,
+                child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: OutlinedButton(onPressed: () {},
+                      child: const Text("My Profile")),
+                ),
+              ),
+            ],
         ),
       ),
     );
